3bf28b9ba2b44a586b2f179017aa0cbeef6b03b7
Adam Crownoble <adam@codenoble.com>
2016-06-17 14:02:01 +0800

    Better testing

--- spec/lib/active_model_serializers/adapter/normalizr_spec.rb
+++ spec/lib/active_model_serializers/adapter/normalizr_spec.rb
@@ -7,6 +7,10 @@ describe ActiveModelSerializers::Adapter::Normalizr do
   subject { ActiveModelSerializers::SerializableResource.new(serializee).as_json }
   let(:result) { subject['result'] }
   let(:entities) { subject['entities'] }
+  let(:articles) { entities['articles'] }
+  let(:comments) { entities['comments'] }
+  let(:likes) { entities['likes'] }
+  let(:photos) { entities['photos'] }
 
   before do
     Article.delete_all
@@ -18,15 +22,24 @@ describe ActiveModelSerializers::Adapter::Normalizr do
   context 'when a model' do
     describe '#serializable_hash' do
       context 'without associated models' do
-        it 'returns a normalizr-like hash' do
+        it 'returns a hash' do
           expect(subject).to be_a Hash
+        end
+
+        it 'contains a result' do
           expect(subject).to have_key 'result'
           expect(result).to eql article.id
+        end
+
+        it 'contains entries' do
           expect(subject).to have_key 'entities'
           expect(entities).to have_key 'articles'
-          expect(entities['articles'].keys.length).to be 1
-          expect(entities['articles'][article.id]['title']).to eql 'Dunno'
-          expect(entities['articles'][article.id]['content']).to eql 'Lorem ipsum...'
+        end
+
+        it 'includes the model attributes' do
+          expect(articles.keys.length).to be 1
+          expect(articles[article.id]['title']).to eql 'Dunno'
+          expect(articles[article.id]['content']).to eql 'Lorem ipsum...'
         end
       end
 
@@ -35,8 +48,30 @@ describe ActiveModelSerializers::Adapter::Normalizr do
 
         it 'serializes the associated model' do
           expect(entities).to have_key 'photos'
-          expect(entities['photos']).to have_key photo.id
-          expect(entities['photos'][photo.id]['url']).to eql 'http://lorempixel.com/100/100/'
+          expect(photos).to have_key photo.id
+          expect(photos[photo.id]['url']).to eql 'http://lorempixel.com/100/100/'
+        end
+      end
+
+      context 'with a has_many association' do
+        context 'when the association has a serializer' do
+          let!(:comment) { Comment.create! article: article, comment: 'Meh' }
+
+          it 'serializes the associated model' do
+            expect(entities).to have_key 'comments'
+            expect(comments).to have_key comment.id
+            expect(comments[comment.id]['comment']).to eql 'Meh'
+          end
+        end
+
+        context 'when the association has no serializer' do
+          let!(:like) { Like.create! article: article, username: 'milton' }
+
+          it 'serializes the associated model' do
+            expect(entities).to have_key 'likes'
+            expect(likes).to have_key like.id
+            expect(likes[like.id]['username']).to eql 'milton'
+          end
         end
       end
     end
@@ -50,20 +85,30 @@ describe ActiveModelSerializers::Adapter::Normalizr do
     let(:serializee) { Article.all }
 
     describe '#serializable_hash' do
-      it 'returns a normalizr-like hash' do
+      it 'returns a hash' do
         expect(subject).to be_a Hash
+      end
+
+      it 'contains a result' do
         expect(subject).to have_key 'result'
-        expect(subject['result']).to be_an Array
-        expect(subject['result'].sort).to eql [article_a.id, article_b.id].sort
+        expect(result).to be_an Array
+        expect(result.sort).to eql [article_a.id, article_b.id].sort
+      end
+
+      it 'contains entries' do
         expect(subject).to have_key 'entities'
-        expect(subject['entities']).to have_key 'articles'
-        expect(subject['entities']['articles'].keys.length).to be 2
-        expect(subject['entities']['articles'][article_a.id]['title']).to eql 'One'
-        expect(subject['entities']['articles'][article_a.id]['content']).to eql 'First...'
-        expect(subject['entities']['articles'][article_b.id]['title']).to eql 'Two'
-        expect(subject['entities']['articles'][article_b.id]['content']).to eql 'Second...'
-        expect(subject['entities']['comments'][comment_a.id]['comment']).to eql 'Meh'
-        expect(subject['entities']['comments'][comment_b.id]['comment']).to eql 'Yay'
+        expect(entities).to have_key 'articles'
+      end
+
+      it "includes the first models' attributes" do
+        expect(articles.keys.length).to be 2
+        expect(articles[article_a.id]['title']).to eql 'One'
+        expect(articles[article_a.id]['content']).to eql 'First...'
+      end
+
+      it "includes the second models' attributes" do
+        expect(articles[article_b.id]['title']).to eql 'Two'
+        expect(articles[article_b.id]['content']).to eql 'Second...'
       end
     end
   end
